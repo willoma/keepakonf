bf9c2a1fb52ec4cd37976c79b515d856a389f114
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2023-12-11 20:05:36 +0100

    files watcher: wait on parent if it is missing

--- internal/external/files_watcher.go
+++ internal/external/files_watcher.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	"errors"
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/fsnotify/fsnotify"
+
 	"github.com/willoma/keepakonf/internal/log"
 )
 
@@ -19,74 +21,21 @@ const (
 	FileStatusFile
 	FileStatusDirectory
 	FileStatusNotFound
+	fileStatusParentRemoved
 
 	filesWatcherDedupDelay = 200 * time.Millisecond
 )
 
-type filesWatcherStruct struct {
-	once     sync.Once
-	watcher  *fsnotify.Watcher
-	mu       sync.Mutex
-	dedups   map[string]*fileStatusDedup
-	dirCount map[string]int
-}
+var (
+	filesWatcher = newFilesWatcherFront()
 
-type fileStatusDedup struct {
-	in  chan FileStatus
-	mu  sync.Mutex
-	out []chan FileStatus
-	val FileStatus
-}
-
-var filesWatcher = filesWatcherStruct{
-	dedups:   map[string]*fileStatusDedup{},
-	dirCount: map[string]int{},
-}
-
-func (d *fileStatusDedup) listen() {
-	go func() {
-		timer := time.AfterFunc(filesWatcherDedupDelay, func() {
-			d.mu.Lock()
-			defer d.mu.Unlock()
-			for _, r := range d.out {
-				r <- d.val
-			}
-		})
-
-		for fstatus := range d.in {
-			if fstatus == FileStatusUnknown {
-				continue
-			}
-			d.mu.Lock()
-			d.val = fstatus
-			timer.Reset(filesWatcherDedupDelay)
-			d.mu.Unlock()
-		}
-	}()
-}
-func (d *fileStatusDedup) addReceiver() chan FileStatus {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	receiver := make(chan FileStatus)
-	d.out = append(d.out, receiver)
-	firstValue := d.val
-	go func() {
-		receiver <- firstValue
-	}()
-	return receiver
-}
-
-func (d *fileStatusDedup) removeReceiver(r chan FileStatus) bool {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	for i, rec := range d.out {
-		if rec == r {
-			d.out[i] = d.out[len(d.out)-1]
-			d.out = d.out[:len(d.out)-1]
-		}
-	}
-	return len(d.out) == 0
-}
+	errDirNotExist              = errors.New("directory does not exist")
+	errFileAlreadyWatched       = errors.New("file already watched")
+	errFileNotWatched           = errors.New("file not watched")
+	errFileWatcherCouldNotWatch = errors.New("file watcher could not watch")
+	errFileWatcherWrongTarget   = errors.New("wrong file watcher target")
+	errNotADirectory            = errors.New("not a directory")
+)
 
 func getFileStatus(path string) FileStatus {
 	finfo, err := os.Stat(path)
@@ -104,100 +53,293 @@ func getFileStatus(path string) FileStatus {
 	return FileStatusFile
 }
 
-func (f *filesWatcherStruct) run() {
-	f.once.Do(func() {
-		watcher, err := fsnotify.NewWatcher()
-		if err != nil {
-			log.Error(err, "Could not create file watcher")
-			return
-		}
-		f.watcher = watcher
-
-		go func() {
-			for {
-				select {
-				case event, ok := <-watcher.Events:
-					if !ok {
-						return
-					}
-
-					f.mu.Lock()
-					target, ok := f.dedups[event.Name]
-					f.mu.Unlock()
-					if !ok {
-						continue
-					}
-
-					switch {
-					case event.Has(fsnotify.Create), event.Has(fsnotify.Write):
-						target.in <- getFileStatus(event.Name)
-					case event.Has(fsnotify.Remove), event.Has(fsnotify.Rename):
-						target.in <- FileStatusNotFound
-					default:
-						// Ignore chmod events
-					}
-				case err, ok := <-watcher.Errors:
-					log.Error(err, "Could not monitor files")
-					if !ok {
-						return
-					}
-				}
-			}
-		}()
-	})
+type filesWatcherBack struct {
+	watcher      *fsnotify.Watcher
+	mu           sync.Mutex
+	targets      map[string]chan FileStatus
+	targetsByDir map[string]map[chan FileStatus]struct{}
 }
 
-func (f *filesWatcherStruct) subscribe(path string) chan FileStatus {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	dedup, ok := f.dedups[path]
-	if !ok {
-		dedup = &fileStatusDedup{
-			in:  make(chan FileStatus),
-			val: getFileStatus(path),
-		}
-
-		dir := filepath.Dir(path)
-		if f.dirCount[dir] == 0 {
-			if err := f.watcher.Add(dir); err != nil {
-				// TODO If directory does not exist, listen for its parent, etc
-				log.Errorf(err, "Could not add directory %q to files watcher", dir)
-			}
-		}
-		dedup.listen()
-		f.dirCount[dir]++
-		f.dedups[path] = dedup
+func newFilesWatcherBack() *filesWatcherBack {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Error(err, "Could not create file watcher")
 	}
 
-	return dedup.addReceiver()
+	return &filesWatcherBack{
+		watcher:      watcher,
+		targets:      map[string]chan FileStatus{},
+		targetsByDir: map[string]map[chan FileStatus]struct{}{},
+	}
 }
 
-func (f *filesWatcherStruct) unsubscribe(path string, receiver chan FileStatus) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	close(receiver)
-	dedup, ok := f.dedups[path]
+func (b *filesWatcherBack) watch() {
+	for {
+		select {
+		case event, ok := <-b.watcher.Events:
+			if !ok {
+				return
+			}
+
+			b.forwardStatus(event)
+		case err, ok := <-b.watcher.Errors:
+			log.Error(err, "Could not monitor files")
+			if !ok {
+				return
+			}
+		}
+	}
+}
+
+func (b *filesWatcherBack) forwardStatus(event fsnotify.Event) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if event.Has(fsnotify.Remove) {
+		if targetDir, ok := b.targetsByDir[event.Name]; ok {
+			for t := range targetDir {
+				t <- fileStatusParentRemoved
+			}
+		}
+	}
+
+	target, ok := b.targets[event.Name]
 	if !ok {
 		return
 	}
-	if dedup.removeReceiver(receiver) {
-		close(dedup.in)
-		delete(f.dedups, path)
+
+	switch {
+	case event.Has(fsnotify.Create), event.Has(fsnotify.Write):
+		target <- getFileStatus(event.Name)
+	case event.Has(fsnotify.Remove), event.Has(fsnotify.Rename):
+		target <- FileStatusNotFound
 	}
-	dir := filepath.Dir(path)
-	f.dirCount[dir]--
-	if f.dirCount[dir] == 0 {
-		if err := f.watcher.Remove(dir); err != nil {
-			log.Errorf(err, "Could not remove directory %q from files watcher", dir)
+}
+
+func (b *filesWatcherBack) addTarget(path string) (chan FileStatus, error) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	if _, ok := b.targets[path]; ok {
+		return nil, errFileAlreadyWatched
+	}
+
+	target := make(chan FileStatus)
+
+	dirPath := filepath.Dir(path)
+	if len(b.targetsByDir[dirPath]) == 0 {
+		if err := b.watcher.Add(dirPath); err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				return nil, errDirNotExist
+			}
+			return nil, fmt.Errorf("could not add directory %q to files watcher: %w", dirPath, err)
 		}
+		b.targetsByDir[dirPath] = map[chan FileStatus]struct{}{target: {}}
+	} else {
+		b.targetsByDir[dirPath][target] = struct{}{}
+	}
+
+	b.targets[path] = target
+
+	firstValue := getFileStatus(path)
+	go func() {
+		target <- firstValue
+	}()
+
+	return target, nil
+}
+
+func (b *filesWatcherBack) removeTarget(path string, target chan FileStatus) error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	currentTarget, ok := b.targets[path]
+	if !ok {
+		return errFileNotWatched
+	}
+	if currentTarget != target {
+		return errFileWatcherWrongTarget
+	}
+
+	dirPath := filepath.Dir(path)
+
+	delete(b.targets, path)
+
+	delete(b.targetsByDir[dirPath], target)
+	if len(b.targetsByDir[dirPath]) == 0 {
+		if err := b.watcher.Remove(dirPath); err != nil {
+			return fmt.Errorf("could not remove directory %q from files watcher: %w", dirPath, err)
+		}
+		delete(b.targetsByDir, dirPath)
+	}
+
+	return nil
+}
+
+type fileWatcherMiddle struct {
+	path  string
+	front *filesWatcherFront
+
+	mu  sync.Mutex
+	in  chan FileStatus
+	out []chan FileStatus
+	val FileStatus
+}
+
+func newFilesWatcherMiddle(path string, front *filesWatcherFront) *fileWatcherMiddle {
+	m := &fileWatcherMiddle{
+		path:  path,
+		front: front,
+	}
+	go m.run()
+	return m
+}
+
+func (m *fileWatcherMiddle) run() {
+	for {
+		err := m.watchFile()
+		switch {
+		case errors.Is(err, errDirNotExist):
+			m.mu.Lock()
+			for _, t := range m.out {
+				t <- FileStatusNotFound
+			}
+			m.mu.Unlock()
+			m.waitForParent()
+		case errors.Is(err, errFileWatcherCouldNotWatch):
+			// Try again later...
+			time.Sleep(10 * time.Second)
+		default:
+			return
+		}
+	}
+}
+
+func (m *fileWatcherMiddle) watchFile() error {
+	target, err := m.front.back.addTarget(m.path)
+	if err != nil {
+		if errors.Is(err, errDirNotExist) {
+			return errDirNotExist
+		}
+		log.Errorf(err, "Could not watch for %q", m.path)
+		return errFileWatcherCouldNotWatch
+	}
+	m.in = target
+
+	timer := time.AfterFunc(filesWatcherDedupDelay, func() {
+		m.mu.Lock()
+		defer m.mu.Unlock()
+		for _, tgt := range m.out {
+			tgt <- m.val
+		}
+	})
+
+	for fstatus := range m.in {
+		if fstatus == fileStatusParentRemoved {
+			return errDirNotExist
+		}
+		if fstatus == FileStatusUnknown {
+			continue
+		}
+		m.mu.Lock()
+		m.val = fstatus
+		timer.Reset(filesWatcherDedupDelay)
+		m.mu.Unlock()
+	}
+	return nil
+}
+
+func (m *fileWatcherMiddle) waitForParent() {
+	dir := filepath.Dir(m.path)
+	tgt := m.front.subscribe(dir)
+	for fstatus := range tgt {
+		switch fstatus {
+		case FileStatusDirectory:
+			m.front.unsubscribe(dir, tgt)
+			return
+		case FileStatusFile:
+			log.Errorf(errNotADirectory, "Could not wait on parent %q creation", dir)
+		}
+	}
+}
+
+func (m *fileWatcherMiddle) addTarget() chan FileStatus {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	target := make(chan FileStatus)
+	m.out = append(m.out, target)
+
+	latestValue := m.val
+	go func() {
+		target <- latestValue
+	}()
+
+	return target
+}
+
+// removeTarget returns true if there is no more targets in this watcher
+func (m *fileWatcherMiddle) removeTarget(target chan FileStatus) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	for i, tgt := range m.out {
+		if tgt == target {
+			m.out[i] = m.out[len(m.out)-1]
+			m.out = m.out[:len(m.out)-1]
+		}
+	}
+
+	if len(m.out) == 0 {
+		m.front.back.removeTarget(m.path, m.in)
+		return true
+	}
+
+	return false
+}
+
+type filesWatcherFront struct {
+	mu      sync.Mutex
+	middles map[string]*fileWatcherMiddle
+	back    *filesWatcherBack
+}
+
+func newFilesWatcherFront() *filesWatcherFront {
+	back := newFilesWatcherBack()
+	go back.watch()
+	return &filesWatcherFront{
+		middles: map[string]*fileWatcherMiddle{},
+		back:    back,
+	}
+}
+
+func (f *filesWatcherFront) subscribe(path string) chan FileStatus {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	middle, ok := f.middles[path]
+	if !ok {
+		middle = newFilesWatcherMiddle(path, f)
+		f.middles[path] = middle
+	}
+	return middle.addTarget()
+}
+
+func (f *filesWatcherFront) unsubscribe(path string, target chan FileStatus) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	middle, ok := f.middles[path]
+	if !ok {
+		return
+	}
+
+	if middle.removeTarget(target) {
+		delete(f.middles, path)
 	}
 }
 
 // WatchFile allows watching for files creation, change or removal, and
 // differentiates files and directories.
 func WatchFile(path string) (target <-chan FileStatus, remove func()) {
-	filesWatcher.run()
-
 	targetChan := filesWatcher.subscribe(path)
 
 	return targetChan, func() {
